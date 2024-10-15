b1b182bd655691d7765a0babd91a0eb3bd764a6b
SHIVAM-ji-stack <vsshmishra345@gmail.com>
2024-10-15 17:24:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,25 @@ function speak(text){
     window.speechSynthesis.speak(text_speak)
 }
 
-function wishMe(){
-    let day=new Date()
-    let hours=day.getHours()
-    if(hours>=0 && hours<12){
-        speak("Good Morning Sir")
-    }
-    else if(hours>=12 && hours<16){
-        speak("Good Afternoon Sir")
-    }
-    else{
-        speak("Good Evening Sir")
-    }
-}
-if (btn) {
-    btn.addEventListener('click', wishMe)
-}
-window.addEventListener('load',() => {
-    wishMe()
-})
+//function wishMe(){
+    //let day=new Date()
+   // let hours=day.getHours()
+   // if(hours>=0 && hours<12){
+       // speak("Good Morning Sir")
+    //}
+    //else if(hours>=12 && hours<16){
+      //  speak("Good Afternoon Sir")
+    //}
+    //else{
+      //  speak("Good Evening Sir")
+   // }}
+
+//if (btn) {
+ //   btn.addEventListener('click', wishMe)
+//}
+//window.addEventListener('load',() => {
+  //  wishMe()
+//})
 let speechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition
 let recognition =new speechRecognition()
 recognition.onresult=(event)=>{
